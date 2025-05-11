60d67012ffeae822528fb4d87f503bea585775eb
syeduroojkamal <syeduroojkamal26@gmail.com>
2025-05-11 14:46:17 +0530

    migrate server to HTTPS with SSL certificate configuration

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 import express from "express";
-import dotenv from "dotenv";
+import fs from "fs";
+import https from "https";
+
 import homeRoutes from "./routes/home.js";
 import logger from "./middleware/logger.js";
 import errorHandler from "./middleware/errorHandler.js";
 
-dotenv.config();
-
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // for JSON body parsing
 app.use(logger); // custom logger
 app.use("/", homeRoutes); // routes
 app.use(errorHandler); // error handler
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+// SSL certificate paths (from certbot)
+const sslOptions = {
+  key: fs.readFileSync(
+    "/etc/letsencrypt/live/ws.chat-system.space/privkey.pem"
+  ),
+  cert: fs.readFileSync(
+    "/etc/letsencrypt/live/ws.chat-system.space/fullchain.pem"
+  ),
+};
+
+https.createServer(sslOptions, app).listen(443, () => {
+  console.log(`🔐 HTTPS Server running at https://ws.chat-system.space`);
 });
